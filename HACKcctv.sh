57ce3dd1f1a9c6435594421fcebb890c605c4c86
INDONESIANtrack5 <67874169+INDONESIANtrack5@users.noreply.github.com>
2020-07-07 11:48:14 +0700

    Create HACKcctv.sh

---
+++ HACKcctv.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+clear
+apt install screenfetch
+screenfetch -A "Linux"
+sleep 2
+echo "ATHOR : Ebeck kansas Rel"
+echo "VERSI : 0.2"
+echo "INDONESIAN TRACK5"
+echo "Github: httow://github.com/INDONESIANtrack5"
+sleep 4
+read -p "NAMAMU SIAPA? " nama
+echo "OK" $nama
+sleep 2
+echo "OTW AMBIL DATA: https://axis.com"
+sleep 3
+clear
+screenfetch -A "Debian"
+echo "Target:"
+sleep 2
+echo "Ip:"
+echo "indonesia:"
+echo "[ http://203.130.227.59:8083 ]"
+echo "[ http://203.130.227.59:8084 ]"
+echo "[ http://203.201.173.172:60001 ]"
+echo "[ http://103.19.229.70:8090 ]"
+echo "[ http://119.2.50.114:86 ]"
+
+#Virus/ziaomzmzxzmsmzkzmzzks
+#jangan di recode berkayalah sendiri bos...
+#CCTV : IP.2.38392.19
+#https://xxnxnxkxkksmk.com
